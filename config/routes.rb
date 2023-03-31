5329b0d5463307565ecae412efb5251bf500137a
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-03-31 12:42:50 +0300

    live sever

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete "/logout", to: "sessions#destroy"
   get '/me', to: 'users#show_current_user'
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
