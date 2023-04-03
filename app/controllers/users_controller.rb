b0168ed6104cddd388edc0ba618f776ad53f11a3
KENNETH <kenneth.mburu@student.moringaschool.com>
2023-04-03 14:59:38 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,41 @@
 class UsersController < ApplicationController
-
-    def create
+      def create
         user = User.create(user_params)
+        puts "User params: #{user_params}"
+        puts "User errors: #{user.errors.full_messages}"
         if user.valid?
           render json: user, status: :created
         else
           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
         end
       end
+      def show_current_user
+        render json: current_user
+      end
+      
     
       private
     
       def user_params
         params.permit(:username, :password, :password_confirmation)
       end
-    # def create
-    #     user = User.create(user_params)
-    #     if user.valid?
-    #       render json: user, status: :created
-    #     else
-    #       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
-    #     end
-    #   end
-    
-    #   private
-    
-    #   def user_params
-    #     params.permit(:username, :password, :password_confirmation)
-    #   end
 end
+
+
+# class UsersController < ApplicationController
+
+#     def create
+#         user = User.create(user_params)
+#         if user.valid?
+#           render json: user, status: :created
+#         else
+#           render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+#         end
+#       end
+    
+#       private
+    
+#       def user_params
+#         params.permit(:username, :password, :password_confirmation)
+#       end
+# end
